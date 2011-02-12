d8c50545de4cc328914c2ff3381d3be3f7483c48
Jared Steele <jasteele@mines.edu>
2011-02-12 13:07:41 -0700

    Adding project 5 starting point. YAY...

---
+++ project05/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'ebca1dc113d9e9ddc2959a71e85b7c9669ae7cc664006b3955ece2648d86e80bad27fab16c744a30e171aa6fd764d40cc0f7b3b5f097a84356441ece443dd2dd';
