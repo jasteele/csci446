5ad286041b23aa2a77b9e4db036125ad9e035f5b
Jared Steele <jasteele@mines.edu>
2011-03-06 01:22:15 -0700

    added red error to flash, however need to change in many places now

--- project06/app/helpers/application_helper.rb
+++ project06/app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+	#got this online from http://snippets.dzone.com/posts/show/1349
+	def flash_div *keys
+	  keys.collect { |key| content_tag(:div, flash[key],
+									   :class => "flash#{key}") if flash[key] }.join
+	end
 end
